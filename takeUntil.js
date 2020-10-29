2d6eedc3acfebfea863916fa8af917a0b00d2376
James Richardson <jamesrichardson604@gmail.com>
2020-10-29 22:38:11 +0000

    W1 D4 - Implement takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const eqArrays = function(actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    return true;
+  } else {
+    return false;
+  }
+  
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  for (let element of array) {
+    if (!callback(element)) {
+      newArray.push(element);
+    } else {
+      return newArray;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1[0], data1[0]);
+assertArraysEqual(results1[1], data1[1]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2[0], data2[0]);
+assertArraysEqual(results2[1], data2[1]);
