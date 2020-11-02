cc5c31e00df0450b1a89ee94c49a39eacb6cd5ce
James Richardson <jamesrichardson604@gmail.com>
2020-11-02 21:14:31 +0000

    refactored test codes and assert functions using exports and imports

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅✅✅ ' + actual + ' === ' + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🛑🛑🛑 ' + actual  + ' !== ' + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual('Lighthouse Labs', 'Lighthouse Labs');
-assertEqual(1, 1);
-assertEqual(1, 5);
+
+module.exports = assertEqual
--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array){
   return array[0]
-}
+};
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head
