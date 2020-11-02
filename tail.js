d5f2fb0bee497c29878217315bce598ea788f718
James Richardson <jamesrichardson604@gmail.com>
2020-11-02 22:58:09 +0000

    tail + tailTest.js refactored with exports + imports

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-
-};
-
 const tail = function() {
   words.slice(0);
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail
+
+
