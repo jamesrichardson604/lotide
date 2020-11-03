4c430c48036f9e304d9563bd7e684e3ae3ef8a16
James Richardson <jamesrichardson604@gmail.com>
2020-11-03 00:28:43 +0000

    refactored middle.js and added middleTest.js to test function

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = function(middle, expected){  
-  if(JSON.stringify(middle) === JSON.stringify(expected)){
-    return true
-  }else{
-    return false
-  }
-  
-}
-
-const assertArraysEqual = function(middle, expected){
-  if(eqArrays(middle, expected)){
-    console.log(`✅✅✅ ${middle} === ${expected}`);
-  }else{
-    console.log(`🛑🛑🛑 ${middle} !== ${expected}`);
-  }
-
-}
-
 const middle = function(arr){
   let middle = []
   if(arr.length > 2){
@@ -28,9 +10,5 @@ const middle = function(arr){
   return middle
 }
 
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+
+module.exports = middle
